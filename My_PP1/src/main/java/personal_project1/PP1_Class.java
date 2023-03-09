b3675a069427413638447936587260024d2e8844
Whales-Hub <obawhales1@gmail.com>
2023-03-09 15:02:42 -0500

    my-pp1-job

---
+++ My_PP1/src/main/java/personal_project1/PP1_Class.java
@@ -0,0 +1,5 @@
+package personal_project1;
+
+public class PP1_Class {
+
+}
---
+++ My_PP1/src/test/java/personal_project1/PP1_JUnit.java
@@ -0,0 +1,14 @@
+package personal_project1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PP1_JUnit {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
